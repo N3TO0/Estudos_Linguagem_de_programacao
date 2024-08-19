b1233ae1c1a638350d82b091c75b63098093e925
N3TO0 <idervaljose00@gmail.com>
2024-08-19 09:33:31 -0300

    Etapa 2 - bibliotecas

---
+++ Python/Exercícios/Etapa_2/Ex_01.py
@@ -0,0 +1,3 @@
+import emoji
+
+print(emoji.emojize("\nOlá, mundo 🌍 \n"))
---
+++ Python/Exercícios/Etapa_2/Ex_2.py
@@ -0,0 +1,4 @@
+import math
+a=float(input("\nDigite um numero: "))
+
+print("O numero inteiro é: {}".format(math.floor(a)))
