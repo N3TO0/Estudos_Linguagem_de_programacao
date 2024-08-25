19fe4f48c81ba337112d6fa012e8437e6d9697a5
N3TO0 <idervaljose00@gmail.com>
2024-08-24 21:25:57 -0300

    exercicios novos

--- Python/Exercícios/Manipulação_de_Str/Ex_01.py
+++ Python/Exercícios/Manipulação_de_Str/Ex_01.py
@@ -1,4 +1,4 @@
+#CODIGO ESCOLHENDO QUAIS CARACTERES RETORNARAM DA VARIAVEL.
+
 nome="iderval jose de lime neto"
-
 print("\n{}\n".format(nome[0:26:1]))
-
---
+++ Python/Exercícios/Manipulação_de_Str/Ex_04.py
@@ -0,0 +1,5 @@
+#CODIGO TRANSFORMANDO CARACTERES ESPECIFICOS DENTRO DE UMA VARIAVEL E RETORNADO.  
+
+nome="iderval jose de lima neto"
+print(nome.replace('neto', 'show'))
+
