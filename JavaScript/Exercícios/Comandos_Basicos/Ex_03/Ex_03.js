94965b71188e27e80ff2e6da6007efff648aae84
N3TO0 <idervaljose00@gmail.com>
2024-09-26 16:43:40 -0300

    exluir pasta js

--- JavaScript/Exercícios/Comandos_Basicos/Ex_03/Ex_03.js
+++
@@ -1,9 +0,0 @@
-let carro = {
-
-roda: 2
-
-};
-
-carro.portas = 2;
-
-alert(JSON.stringify(carro));
--- JavaScript/Exercícios/Estrutuda_de_dados/Estrutura_de_Repetição/While/Ex_02/Ex_01.js
+++
@@ -1,4 +0,0 @@
-//console.log("Olá Mundo!");
-let num = 10;
-let nome = "neto show"
-alert(nome)
